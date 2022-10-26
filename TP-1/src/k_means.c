d2cf35419c0055f69dc0ead42e6c9bf7d6f0d860
GoncaloCoutoDosSantos <a93279@alunos.uminho.pt>
2022-10-26 05:17:27 +0100

    versao dos stores

--- TP-1/src/k_means.c
+++ TP-1/src/k_means.c
@@ -1,17 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <assert.h>
+
 #include "../include/utils.h"
 
-#define N 1000000
+#define N 10000000
 #define K 4
 
 //return is number of iterarions
 int k_means(float *cluster_x,float *cluster_y,float *arr_x,float *arr_y,int *n_elem_cluster){
 	float mean_x[K],mean_y[K]; //keep values to calculate new centroid
 	float dist[K]; //auxiliar vector for calculate distance betwen centroid and a point 
+	float *new_points,*old_points;
 	int ret = 0;
 	char flag = 1;
+
+	new_points = malloc(sizeof(float) * N);
+	old_points = calloc(sizeof(float) , N);
 
 	while(flag){
 		//reset means and n_elem_cluster for cluster calculation
@@ -21,14 +27,21 @@ int k_means(float *cluster_x,float *cluster_y,float *arr_x,float *arr_y,int *n_e
 			n_elem_cluster[i] = 0;
 		}
 
+		flag = 0;
+
 		//primary loop calculate the 
 		for(int i = 0; i < N;i++){ 
 			for(int j = 0; j < K;j++){//calculate distance betewn point and centroids 
-				dist[j]  = (cluster_x[j] - arr_x[i]) * (cluster_x[j] - arr_x[i]) + (cluster_y[j] - arr_y[i]) * (cluster_y[j] - arr_y[i]); 
+				dist[j]  = (cluster_x[j] - arr_x[i]) * (cluster_x[j] - arr_x[i]); 
+				dist[j] += (cluster_y[j] - arr_y[i]) * (cluster_y[j] - arr_y[i]);
 			}
 
 			int ind = 0; //indice da menor distancia
 			for(int j = 1; j < K;j++) ind = (dist[j] < dist[ind]) ?j:ind;
+			new_points[i] = ind;
+
+			flag = (old_points[i] != ind)?1:flag;
+
 			mean_x[ind] += arr_x[i];
 			mean_y[ind] += arr_y[i];
 			n_elem_cluster[ind]++; 
@@ -36,22 +49,23 @@ int k_means(float *cluster_x,float *cluster_y,float *arr_x,float *arr_y,int *n_e
 
 		//new centroids calculations 
 		for(int i = 0; i < K;i++){
-			mean_x[i] = mean_x[i] / ((float)n_elem_cluster[i]);
-		}
-		for(int i = 0;i < k;i++){
-			mean_y[i] = mean_y[i] / ((float)n_elem_cluster[i]);
+			cluster_x[i] = mean_x[i] / ((float)n_elem_cluster[i]);
+			cluster_y[i] = mean_y[i] / ((float)n_elem_cluster[i]);
 		}
 
-		flag = 0;
-		for(int i = 0; i < K;i++){
-			if(mean_x[i] != cluster_x[i] || mean_y[i] != cluster_y[i]) {flag = 1;}
-			cluster_x[i] = mean_x[i];
-			cluster_y[i] = mean_y[i];
-		}
+		float *aux = new_points;
+		new_points = old_points;
+		old_points = aux;
 
 		ret++;
 
 	}
+
+
+	free(new_points);
+	free(old_points);
+
+
 	return ret;
 }
 
