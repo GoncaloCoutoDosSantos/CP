690b3a3507413dd6f9d674ec0e5bdcbe3dd16f0b
GoncaloCoutoDosSantos <a93279@alunos.uminho.pt>
2022-11-13 19:21:34 +0000

    utils fix

--- TP-2/src/utils.c
+++ TP-2/src/utils.c
@@ -16,7 +16,7 @@ void init(int n_elem,int n_k,float *arr_x,float *arr_y,float *cluster_x,float *c
 void print_ret(float *cluster_x,float *cluster_y,int *n_elem_cluster,int size,int n_k,int iterarion){
 	printf("N = %d, K = %d\n",size,n_k);
 	for(int i = 0;i < n_k;i++){
-		printf("Center: (%.3f, %.16f) : Size: %d\n",cluster_x[i],cluster_y[i],n_elem_cluster[i]);
+		printf("Center: (%.3f, %.3f) : Size: %d\n",cluster_x[i],cluster_y[i],n_elem_cluster[i]);
 	}
 	printf("Iterations: %d\n",iterarion);
 }
